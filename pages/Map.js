41b380ee846151f0a45e4c93a8677ff5986cfb98
Snuggleapp <143851563+Snuggleapp@users.noreply.github.com>
2023-11-13 11:05:37 -0300

    teste map

--- pages/Map.js
+++ pages/Map.js
@@ -1,33 +1,47 @@
 import React, { useEffect, useState } from "react";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Text } from "react-native";
+import * as Location from "expo-location";
 
 export default function Map() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -23.533773,
-          longitude: -46.625290,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: -23.533773,
-            longitude: -46.625290,
-          }}
-          title="Localização"
-          description="Você está aqui"
-        />
-      </MapView>
+      {location ? (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={region}
+          showsUserLocation={true}
+        >
+        </MapView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
-
 }
 
 const styles = StyleSheet.create({
@@ -35,10 +49,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: "100%",
-    height: "100%",
+    ...StyleSheet.absoluteFillObject,
   },
 });
+
+
+
 
